78f2d05a99341ee32099a847bb24fcfe6f394fb5
savasturk <pinar.savasturk@ceng.deu.edu.tr>
2023-06-22 14:40:28 +0300

    GET and POST request examples applied in section 4.

---
+++ bookDemo/Controllers/BooksController.cs
@@ -0,0 +1,51 @@
+﻿using bookDemo.Data;
+using bookDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookDemo.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = ApplicationContext.Books;
+            return Ok(books);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name ="id")] int id)
+        {
+            var book = ApplicationContext
+                .Books
+                .Where(b => b.Id.Equals(id))
+                .SingleOrDefault();
+            if(book is null)
+                return NotFound(); //404
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody]Book book)
+        {
+            try
+            {
+                if(book is null)
+                {
+                    return BadRequest(); //400
+                }
+                ApplicationContext.Books.Add(book);
+                return StatusCode(201, book);
+            }
+            catch (Exception ex)
+            {
+
+               return BadRequest(ex.Message);
+            }
+        }
+    }
+}
